ba30b0056bcf370344dee2a40f8ac393108d3fb2
James Elgar <jamesnelgar@gmail.com>
2021-10-17 20:32:30 +0100

    Switch to attr

--- acicd/provider.py
+++ acicd/provider.py
@@ -1,57 +1,86 @@
 from __future__ import annotations
 from typing import Optional
 
-from pydantic import BaseModel
+import attr
 import yaml
 
 # Very github centric atm
 
 
-class Trigger(BaseModel):
+def is_not_null(instance, attribute, value):
+    return not value is None
+
+
+@attr.s
+class Trigger:
     pass
 
 
-class Env(BaseModel):
-    name: str
-    value: str
+@attr.s
+class Env:
+    name: str = attr.ib()
+    value: str = attr.ib()
 
 
-class Step(BaseModel):
-    name: str
-
-
-class RunStep(Step):
-    run: str
-
-
-class UsesStep(Step):
-    uses: str
-
-
-class Job(BaseModel):
-    name: str
-    steps: Optional[list[Step]] = None
-    image: Optional[str] = None
-    dependencies: Optional[list[Job]] = None
+@attr.s
+class Step:
+    name: str = attr.ib()
 
     def json(self):
-        return {
-            "runs-on": self.image,
-            "steps": [step.json() for step in self.steps] if self.steps else None,
-        }
+        return {"name": self.name}
 
 
-class Workflow(BaseModel):
-    name: Optional[str] = None
-    on: Optional[list[Trigger]] = None
-    env: Optional[list[Env]] = None
-    jobs: Optional[list[Job]] = None
+@attr.s
+class RunStep(Step):
+    run: str = attr.ib()
+
+    def json(self):
+        step_json = super().json()
+        return step_json | {"run": self.run}
+
+
+@attr.s
+class UsesStep(Step):
+    uses: str = attr.ib()
+
+    def json(self):
+        step_json = super().json()
+        return step_json | {"uses": self.uses}
+
+
+@attr.s
+class Job:
+    name: str = attr.ib(validator=[is_not_null])
+    steps: Optional[list[Step]] = attr.ib(default=None)
+    image: Optional[str] = attr.ib(default=None)
+    dependencies: Optional[list[Job]] = attr.ib(default=None)
+
+    def json(self):
+        output = {}
+        if self.image:
+            output["runs-on"] = self.image
+
+        if self.steps:
+            output["steps"] = [step.json() for step in self.steps]
+
+        if self.dependencies:
+            output["needs"] = [dep.name for dep in self.dependencies]
+
+        return output
+
+
+@attr.s
+class Workflow:
+    name: Optional[str] = attr.ib()
+    on: Optional[list[Trigger]] = attr.ib(default=None)
+    env: Optional[list[Env]] = attr.ib(default=None)
+    jobs: Optional[list[Job]] = attr.ib(default=None)
 
     def json(self):
         return {
             "name": self.name,
             "env": {var.name: var.value for var in self.env or []},
-            "jobs": {job.name: job.json() for job in self.jobs or []},
+            "jobs": {job.name: job.json() for job in self.jobs or [] if job.json()},
         }
 
     def yaml(self, file_name):
--- playground.py
+++ playground.py
@@ -1,6 +1,10 @@
-from acicd.provider import Workflow, Env, Job
+from acicd.provider import Workflow, Env, Job, RunStep
 
-jobs = [Job(name="Job 101"), Job(name="Job 102")]
+job_101_steps = [RunStep(name="Run step", run="echo 'Hello world'")]
+job_101 = Job(name="Job 101", steps=job_101_steps)
+
+jobs = [job_101, Job(name="Job 102", dependencies=[job_101, job_101])]
 w = Workflow(name="Test workflow", env=[Env(name="abc", value="def")], jobs=jobs)
+
 print(w.json())
 print(w.yaml("test_output.yml"))
